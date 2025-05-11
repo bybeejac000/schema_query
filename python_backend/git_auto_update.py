6e98ebf06cc786dc63fccceb57e8b737774abdfd
bybeejac000 <jake.bybee@bybeemail.com>
2025-05-11 15:02:44 -0600

    webhook

---
+++ python_backend/git_auto_update.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/update', methods=['POST'])
+def update():
+    subprocess.run(['git', 'pull'], cwd='/home/ubuntu/your-repo-path')
+    return 'Updated!', 200
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=6000)
