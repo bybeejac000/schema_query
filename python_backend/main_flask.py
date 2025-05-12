814b50c29924d6723a686bd33e60abb49c0d22fb
bybeejac000 <jake.bybee@bybeemail.com>
2025-05-11 18:22:31 -0600

    change port

--- python_backend/main_flask.py
+++ python_backend/main_flask.py
@@ -21,6 +21,8 @@ def query():
     return jsonify({'response': response})
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
 
